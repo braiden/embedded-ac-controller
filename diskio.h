1efa716f387a3fc9262da5cac6f3164ae302f878
Braiden Kindt <braiden@braiden.org>
2010-07-24 21:08:42 +0000

    - copywrites
    git-svn-id: http://braiden.org/svn/trunk/projects/lgv3@465 1cc42611-e091-430a-a3cd-04b17827a907

--- diskio.h
+++ diskio.h
@@ -1,6 +1,24 @@
-/*-----------------------------------------------------------------------
-/  PFF - Low level disk interface modlue include file    (C)ChaN, 2009
-/-----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------/
+/  Petit FatFs - FAT file system module  R0.02                 (C)ChaN, 2009
+/-----------------------------------------------------------------------------/
+/ Petit FatFs module is an open source software to implement FAT file system to
+/ small embedded systems. This is a free software and is opened for education,
+/ research and commercial developments under license policy of following trems.
+/
+/  Copyright (C) 2009, ChaN, all right reserved.
+/
+/ * The Petit FatFs module is a free software and there is NO WARRANTY.
+/ * No restriction on use. You can use, modify and redistribute it for
+/   personal, non-profit or commercial use UNDER YOUR RESPONSIBILITY.
+/ * Redistributions of source code must retain the above copyright notice.
+/
+/-----------------------------------------------------------------------------/
+/ Jun 15,'09  R0.01a  First release. (Branched from FatFs R0.07b.)
+/
+/ Dec 14,'09  R0.02   Added multiple code page support.
+/                     Added write funciton.
+/                     Changed stream read mode interface.
+/----------------------------------------------------------------------------*/
 
 #ifndef _DISKIO
 
